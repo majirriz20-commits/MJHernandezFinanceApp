38b36e0bfb3e456199a6bc96feb55abcb1e73b9b
majirriz20-commits <majirriz20@gmail.com>
2026-03-12 19:09:48 -0600

    Transacciones

--- app/src/main/java/com/example/mjfinanceapp/MainActivity.kt
+++ app/src/main/java/com/example/mjfinanceapp/MainActivity.kt
@@ -5,20 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -27,13 +34,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mjfinanceapp.ui.theme.MJFinanceAppTheme
 import models.SumaryCard
+import models.Transaction
 import models.User
+import models.listaSumaryCard
+import models.listaTransacciones
+import models.listaUsuario
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,11 +63,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyComplexLayout(innerPadding: PaddingValues) {
-    val usuario = User(1, "Maria", 1500.99)
-    val cards = listOf(
-        SumaryCard("Ventas", 1500.99),
-        SumaryCard("Ventas", 1500.99)
-    )
 
     Column(
         modifier = Modifier
@@ -64,8 +71,21 @@ fun MyComplexLayout(innerPadding: PaddingValues) {
             .background(Color.White)
             .padding(horizontal = 15.dp, vertical = 30.dp)
     ) {
-        Bienvenida(user = usuario)
-        Cards(sumaryCard = cards)
+        //Bienvenida
+        listaUsuario.forEach { user ->
+            Bienvenida(user = user)
+        }
+
+        //CARDS
+        Cards(sumaryCard = listaSumaryCard)
+
+        //TRANSACCION
+        TransactionsHeader()
+        listaTransacciones.forEach { transaction ->
+            TransaccionesItem(
+                transaction = transaction
+            )
+        }
     }
 }
 
@@ -226,7 +246,86 @@ fun Cards(sumaryCard: List<SumaryCard>){
 
 //Transactions
 
+//Transactions header
+@Composable
+fun TransactionsHeader(){
+    //Encabezado
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 15.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Transactions",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "See all",
+            fontSize = 16.sp,
+            color = Color.Gray
+        )
+    }
+}
 
+@Composable
+fun TransaccionesItem(transaction: Transaction){
+    //Items
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+                .background(Color.Black),
+            contentAlignment = Alignment.Center
+        ){
+            Icon(
+                imageVector = Icons.Default.ShoppingCart,
+                contentDescription = "Carritos de compras",
+                tint = Color.White,
+                modifier = Modifier
+                    .size(24.dp)
+            )
+        }
+        Column(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Text(
+                text = transaction.establecimiento,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = transaction.categoria,
+                fontSize = 14.sp,
+                color = Color.Gray
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.End
+        ) {
+            Text(
+                text = "$${transaction.monto}",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = transaction.hora,
+                fontSize = 12.sp,
+                color = Color.Gray
+            )
+        }
+    }
+
+}
 
 
 @Preview(showBackground = true, showSystemUi = true)
